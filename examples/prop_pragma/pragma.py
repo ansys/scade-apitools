eac12c3b93188b11068816ef983223a926850ce5
Jean Henry <jean.henry@ansys.com>
2023-11-06 15:15:29 +0100

    Migrate to the version 0.2.0 of add-license-headers and reactivate the hook.

--- examples/prop_pragma/pragma.py
+++ examples/prop_pragma/pragma.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2023 ANSYS, Inc. and/or its affiliates.
+
 """
 Example for the basic functions of ansys.scade.apitools.prop.pragma.
 
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2023 ANSYS, Inc. and/or its affiliates.
+
 from ansys.scade.apitools import __version__
 
 
