ffd330d105c070727220721ab65cf44b76061c05
klmcadams <kerry.mcadams@ansys.com>
2024-01-16 11:55:06 -0500

    update headers and pre-commit config file

--- src/ansys/scade/apitools/create/scade.py
+++ src/ansys/scade/apitools/create/scade.py
@@ -1,5 +1,4 @@
-# Copyright (C) 2023 ANSYS, Inc. and/or its affiliates.
-# SPDX-FileCopyrightText: 2023 ANSYS, Inc. All rights reserved.
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
 # SPDX-License-Identifier: MIT
 #
 #
