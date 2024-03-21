3db29faeda2af37cd534c7ceee75a70a27d351fe
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-03-21 08:47:18 +0100

    fix: test the apidoc

--- doc/make.bat
+++ doc/make.bat
@@ -10,13 +10,6 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=_build
 
-REM TODO: these lines of code should be removed once the feature branch is merged
-for /f %%i in ('pip freeze ^| findstr /c:"sphinx-autoapi @ git+https://github.com/ansys/sphinx-autoapi"') do set is_custom_sphinx_autoapi_installed=%%i
-if NOT "%is_custom_sphinx_autoapi_installed%" == "sphinx-autoapi" (
-	python -m pip uninstall --yes sphinx-autoapi
-	python -m pip install "sphinx-autoapi @ git+https://github.com/ansys/sphinx-autoapi@feat/single-page-stable")
-REM TODO: these lines of code should be removed once the feature branch is merged
-
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
 
