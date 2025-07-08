4c1cd30cc4a06058761091adac77c147f9940dc7
Jean Henry <126060567+ansjhenry@users.noreply.github.com>
2025-07-08 08:11:24 +0200

    fix: resolve linter errors

--- doc/source/create/res/create_type/debug_create_type.py
+++ doc/source/create/res/create_type/debug_create_type.py
@@ -22,6 +22,7 @@ if target_dir.exists():
 copytree(source_dir, target_dir)
 
 # declare the duplicated model
+assert declare_project  # nosec
 declare_project(str(target_dir / 'Model.etp'))
 
 # regular script
